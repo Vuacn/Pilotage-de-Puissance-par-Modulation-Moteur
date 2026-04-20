c00a43f35f282b55401724c28e95909f76378d5a
Vuacn <vuactsim@gmail.com>
2026-04-20 15:04:40 +0200

    Translate comments to English in code.ino

--- code.ino
+++ code.ino
@@ -1,11 +1,11 @@
 // Arduino C++ code
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(0x20, 16, 2); // Adresse I2C : 0x20 or 0x27 ( iused 0x27)
-int brochePot = A0; // Entrée analogique
-int brocheMoteur = 9; // Sortie PWM (notée ~ sur Arduino)
-int valeurCAN = 0; // De 0 à 1023
-int valeurPWM = 0; // De 0 à 255
+LiquidCrystal_I2C lcd(0x20, 16, 2); // Address I2C : 0x20 or 0x27 ( i used 0x27)
+int brochePot = A0; // analogic enter
+int brocheMoteur = 9; // exit of the PWM (noted with ~ on the Arduino)
+int valeurCAN = 0; //  0 to 1023
+int valeurPWM = 0; //  0 to 255
 float tension = 0.0;
 int pourcentage = 0;
 void setup() {
@@ -19,11 +19,11 @@ lcd.clear();
 }
 
 void loop() {
-valeurCAN = analogRead(brochePot); // Lecture tension potentiomètre
-valeurPWM = map(valeurCAN, 0, 1023, 0, 255); // Conversion pour analogWrite
-tension = valeurCAN * 5.0 / 1023.0; // Calcul de la tension réelle
-pourcentage = map(valeurCAN, 0, 1023, 0, 100); // Pourcentage de vitesse
-analogWrite(brocheMoteur, valeurPWM); // Génération du signal PWM
+valeurCAN = analogRead(brochePot); //Potentiometer voltage reading
+valeurPWM = map(valeurCAN, 0, 1023, 0, 255); // Conversion for analogWrite
+tension = valeurCAN * 5.0 / 1023.0; // Calculation of the actual voltage
+pourcentage = map(valeurCAN, 0, 1023, 0, 100); // Percentage of speed
+analogWrite(brocheMoteur, valeurPWM); // GPWM signal generation
 // Affichage LCD
 lcd.setCursor(0, 0); lcd.print("CAN: "); lcd.print(valeurCAN);
 lcd.setCursor(0, 1); lcd.print("Vitesse: "); lcd.print(pourcentage); lcd.print("%");
